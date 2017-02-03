ff7b5e6c9e343f234bd5bc642fb42338eb575bc8
Kommotion <nickdanico@aim.com>
2017-02-03 12:40:12 -0500

    Added constants file

---
+++ gui/utils/constants.py
@@ -0,0 +1,13 @@
+ACCEPTABLE_FILETYPES = (
+    'jpeg',
+    'png',
+    'stl',
+    'bmp'
+)
+
+FILE_EXTENSIONS = (
+    ('JPG files', '*.jpg'),
+    ('PNG files', '*.png'),
+    ('BMP files', '*.bmp'),
+    ('STL files', '*.stl'),
+    ('All files', '*.*'))
