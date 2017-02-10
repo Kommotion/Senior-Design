c41e274e50201e539fd98b90bf36a668e5ba0761
Kommotion <nickdanico@aim.com>
2017-02-10 13:49:39 -0500

    Added config.json parsing function

--- gui/utils/parsers.py
+++ gui/utils/parsers.py
@@ -1,3 +1,6 @@
+import json
+import os
+
 
 def conversion(options, key):
     """ Tries to return the value of a given key of given dict
@@ -8,3 +11,15 @@ def conversion(options, key):
         return options[key].get()
     except KeyError:
         return None
+
+
+def get_from_config(key, file_path):
+    """
+    :param key: The key from the config file
+    :param file_path: the main filepath where config is located
+
+    :return: The value associated to the key in the config file
+    """
+    with open(os.path.join(file_path, 'config.json')) as file:
+        data = json.load(file)
+        return data[key]
